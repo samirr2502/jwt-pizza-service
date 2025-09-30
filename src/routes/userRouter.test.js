2addff653a3b0ef2840524ca66e58e010a417fcf
samirr2502 <samirrodriguez2502@gmail.com>
2025-09-29 22:45:11 -0600

    add(userRouter.test) passing with 94% coverage. All lines covered(56.41%)

---
+++ src/routes/userRouter.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const app = require('../service');
+
+
+const testUser = { name: 'testPizza', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+let testUserId;
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+  testUserId = registerRes.body.user.id
+});
+
+test('getUser', async()=>{
+    const getUserRes = await request(app).get('/api/user/me').set('Authorization', `Bearer ${testUserAuthToken}`)
+    expect(getUserRes.status).toBe(200);
+    
+    const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+    delete expectedUser.password;
+    expect(getUserRes.body).toMatchObject(expectedUser);
+
+})
+
+test('updateUser', async()=>{
+    const updateUseRes = await request(app).put(`/api/user/${testUserId}`).set('Authorization', `Bearer ${testUserAuthToken}`).send(data=testUser)
+    expect(updateUseRes.status).toBe(200);
+    
+    const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+    delete expectedUser.password;
+    expect(updateUseRes.body.user).toMatchObject(expectedUser);
+
+})
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
