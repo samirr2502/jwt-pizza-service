a6ca8da8bf739ef5c55e0403073cf1fecd026600
samirr2502 <samirrodriguez2502@gmail.com>
2025-11-12 18:45:36 -0700

    removed unused variable

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,7 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
-const { logger, sendLog } = require('./../logger.js');
+const { sendLog } = require('./../logger.js');
 class DB {
   constructor() {
     this.initialized = this.initializeDatabase();
