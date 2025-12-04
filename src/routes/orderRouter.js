c1cb8cb389e8588f399571abdacda96aed620036
samirr2502 <samirrodriguez2502@gmail.com>
2025-12-03 18:13:34 -0700

    added chaos to orderRouter

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -72,7 +72,25 @@ orderRouter.get(
     res.json(await DB.getOrders(req.user, req.query.page));
   })
 );
+let enableChaos = false;
+orderRouter.put(
+  '/chaos/:state',
+  authRouter.authenticateToken,
+  asyncHandler(async (req, res) => {
+    if (req.user.isRole(Role.Admin)) {
+      enableChaos = req.params.state === 'true';
+    }
 
+    res.json({ chaos: enableChaos });
+  })
+);
+
+orderRouter.post('/', (req, res, next) => {
+  if (enableChaos && Math.random() < 0.5) {
+    throw new StatusCodeError('Chaos monkey', 500);
+  }
+  next();
+});
 // createOrder
 orderRouter.post(
   '/',
